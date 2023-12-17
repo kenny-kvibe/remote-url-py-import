d70cf4ec02a372d89c1ab1439543cf603025637e
~KVibe. <16114808+kenny-kvibe@users.noreply.github.com>
2023-12-17 12:56:59 +0100

    Changed functions structure, same functionality + short comment explanation

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import module_importer
 def test_import():
 	module_importer.init_logger(module_importer.Logger.ERROR)
 
+	# import  "data_app.py"
+	# from    "https://github.com/kenny-kvibe/flask-pandas-app"
+
 	module_importer.add_import_github_repo(
 		'kenny-kvibe', 'flask-pandas-app', 'main')
-
 	import data_app  #type:ignore
+
 	print(data_app.main(5, 5, 0, 9))
 
 	# print(module_importer.imported_modules())
