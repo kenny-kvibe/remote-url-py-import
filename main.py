2dba5978584c3d67c1cac33d222216878e1f5dc8
~KVibe. <16114808+kenny-kvibe@users.noreply.github.com>
2023-12-17 12:55:01 +0100

    Changed functions structure, same functionality

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import module_importer
 
 
 def test_import():
+	module_importer.init_logger(module_importer.Logger.ERROR)
+
 	module_importer.add_import_github_repo(
 		'kenny-kvibe', 'flask-pandas-app', 'main')
+
 	import data_app  #type:ignore
 	print(data_app.main(5, 5, 0, 9))
 
+	# print(module_importer.imported_modules())
+	# print(module_importer.importer_meta_classes())
 
-def main():
-	module_importer.init_logger(module_importer.Logger.ERROR)
+
+def main() -> int:
 	test_import()
-
-	print(module_importer.imported_modules())
-	print(module_importer.importer_meta_classes())
-
+	return 0
 
 
 if __name__ == '__main__':
-	main()
+	raise SystemExit(main())
